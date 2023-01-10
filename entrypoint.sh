84ac165863f37e22b5439ec8a3ae94b53bd29e07
Dylan700 <dylanwilliams700@gmail.com>
2023-01-10 13:52:05 +1100

    Add validation server feature

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,25 @@
 #!/bin/sh -l
+function send_request(){
+	curl "https://www.google-analytics.com/${7}mp/collect?api_secret=$3&measurement_id=$2" -d "{\"client_id\":\"github\",\"non_personalized_ads\":true,\"events\":[{\"name\":\"$1\",\"params\":{\"items\":[],\"description\":\"$6\",\"url\":\"https://github.com/$4/commit/$5\"}}]}"
+}
+
 apk add curl
-echo "Sending the request..."
+apk add jq
 
-curl --fail "https://www.google-analytics.com/mp/collect?api_secret=$3&measurement_id=$2" -d "{\"client_id\":\"github\",\"non_personalized_ads\":true,\"events\":[{\"name\":\"$1\",\"params\":{\"items\":[],\"description\":\"$6\",\"url\":\"https://github.com/$4/commit/$5\"}}]}"
-status=$?
+send_validation=$(send_request "$1" "$2" "$3" "$4" "$5" "$6" "debug/")
+send_request=$(send_request "$1" "$2" "$3" "$4" "$5" "$6")
 
-[ $status -eq 0 ] && echo "Request was successful" || echo "Request failed"
-exit $status
+validation_response=$(echo "$send_validation")
+
+# validate the request
+echo "Validating the request..."
+if [ $(echo "$validation_response" | jq .validationMessages | jq length) -eq 0 ]
+then
+	echo "Request is valid"
+	# make the actual request now
+	echo "Sending the request..."
+	$send_request
+else
+	echo "Request is not valid"
+	exit 1
+fi;
